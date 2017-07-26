9ff4f99fa9819e15c5db9b9d7966e08c0a31c627
Naoyuki Miyazaki <miyazaki@buddying.jp>
2017-07-26 17:08:09 +0900

    カスタムメディア追加

--- markup/config/postcss.config.js
+++ markup/config/postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  parser: 'postcss-scss',
   plugins: {
+    'postcss-easy-import': {},
+    'postcss-custom-media': {},
+    'precss': {},
     'autoprefixer': { browsers: ['last 2 versions'] }
   }
 };
